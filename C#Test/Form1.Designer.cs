554f745d13867ac40e019176240781905a726609
GoldenStarGamer <vicentetavareslima@gmail.com>
2023-03-12 10:57:50 +0000

    Added MD5 and DPIAware

--- C#Test/Form1.Designer.cs
+++ C#Test/Form1.Designer.cs
@@ -32,12 +32,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hashr = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.button1.Location = new System.Drawing.Point(713, 384);
+            this.button1.Location = new System.Drawing.Point(504, 133);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 54);
             this.button1.TabIndex = 0;
@@ -59,27 +63,78 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(667, 36);
+            this.comboBox1.Items.AddRange(new object[] {
+            "MD5",
+            "RIPEMD160",
+            "SHA1",
+            "SHA256",
+            "SHA384",
+            "SHA512"});
+            this.comboBox1.Location = new System.Drawing.Point(458, 36);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 24);
             this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.label2.Location = new System.Drawing.Point(693, 9);
+            this.label2.Location = new System.Drawing.Point(489, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(90, 24);
             this.label2.TabIndex = 3;
             this.label2.Text = "Algorithm";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(567, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label3.Location = new System.Drawing.Point(12, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hashed:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // hashr
+            // 
+            this.hashr.AutoSize = true;
+            this.hashr.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.hashr.Location = new System.Drawing.Point(99, 102);
+            this.hashr.Name = "hashr";
+            this.hashr.Size = new System.Drawing.Size(0, 24);
+            this.hashr.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button2.Location = new System.Drawing.Point(12, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 54);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Hash";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(591, 199);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.hashr);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
@@ -98,6 +153,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label hashr;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- C#Test/Form1.cs
+++ C#Test/Form1.cs
@@ -7,11 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Security.Policy;
 
 namespace C_Test
 {
+    
     public partial class Form1 : Form
     {
+        static string ComputeMD5(string s) {
+            StringBuilder sb = new StringBuilder();
+
+            // Initialize a MD5 hash object
+            using (MD5 md5 = MD5.Create()) {
+                // Compute the hash of the given string
+                byte[] hashValue = md5.ComputeHash(Encoding.UTF8.GetBytes(s));
+
+                // Convert the byte array to string format
+                foreach (byte b in hashValue) {
+                    sb.Append($"{b:X2}");
+                }
+            }
+
+            return sb.ToString();
+        }
+        string algorithm;
+        string tth;
         public Form1()
         {
             InitializeComponent();
@@ -39,6 +60,42 @@ namespace C_Test
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+            algorithm = comboBox1.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+            tth = textBox1.Text;
+        }
+
+        private void label3_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            switch(algorithm) {
+                case "MD5":
+                    hashr.Text = ComputeMD5(tth);
+                    break;
+
+                case "RIPEMD160":
+                    break;
+
+                case "SHA1":
+                    break;
+
+                case "SHA256":
+                    break;
+
+                case "SHA384":
+                    break;
+
+                case "SHA512":
+                    break;
+
+            }
         }
     }
 }
